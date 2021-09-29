b0ac3810ab24fba68aaa672e9f1d05a884feb95b
Степан Захаров <stepanzh@gmail.com>
2021-09-29 17:26:04 +0300

    Adaptive до реализация

--- book/src.jl
+++ book/src.jl
@@ -200,3 +200,19 @@ function romberg(f, a, b; atol=1e-6, maxstep::Integer=100)
     end
     error("Точность не удовлетворена.")
 end
+
+function rombergwstep(f, a, b; atol=1e-6, maxstep::Integer=100)
+    maxstep = max(2, maxstep)
+    I = Matrix{Float64}(undef, maxstep+1, maxstep+1)
+    I[1, 1] = (b - a) * (f(a) + f(b)) / 2
+    for i in 2:maxstep+1
+        let hc = (b - a) / 2^(i-1), np = 2^(i-2)
+            I[i, 1] = I[i-1, 1] / 2 + hc * sum(f, (a + hc * (2i-1) for i in 1:np))
+        end
+        for k in i-1:-1:1
+            I[k, i-k+1] = (2^i*I[k+1, i-k] - I[k, i-k]) / (2^i - 1)
+        end
+        abs(I[1, i] - I[2, i-1]) < atol && return I[1, i], i
+    end
+    error("Точность не удовлетворена.")
+end
