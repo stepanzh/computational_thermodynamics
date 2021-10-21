48ac4871834bbdcafd114f5e114870977197fa0f
Степан Захаров <stepanzh@gmail.com>
2021-10-21 18:12:24 +0300

    ITP method

--- book/src.jl
+++ book/src.jl
@@ -403,3 +403,46 @@ function ridders(f, x₁, x₂; maxiter=25, xtol=eps(), ftol=eps())
     end
     error("Число итераций превышено.")
 end
+
+"""
+Метод ITP поиска корня `f`(x) = 0 c точностью `xtol`.
+"""
+function itproot(f, x₁, x₂; xtol=eps(), ftol=eps(), κ₁=0.1, κ₂=2, n₀=1)
+    if x₁ > x₂; x₁, x₂ = x₂, x₁; end
+    y₁, y₂ = f(x₁), f(x₂)
+    y₁ * y₂ > 0 && error("Функция должна иметь разные знаки в концах отрезка")
+    y₁ == 0 && return x₁
+    y₂ == 0 && return x₂
+
+    nbisect = ceil(Int, log2((x₂-x₁)/xtol))
+    maxiter = nbisect + n₀
+    brackorig = x₂ - x₁
+
+    for i in 1:maxiter
+        # interpolate
+        xf = (y₂*x₁ - y₁*x₂)/(y₂ - y₁)
+
+        # truncate
+        xmid = (x₁ + x₂)/2
+        σ = sign(xmid - xf)
+        δ = κ₁ * (x₂ - x₁)^κ₂ / brackorig
+        xt = δ ≤ abs(xmid - xf) ? xf + copysign(δ, σ) : xmid
+
+        # project
+        r = xtol * 2.0^(maxiter - i) - (x₂ - x₁)/2
+        xnew = abs(xt - xmid) ≤ r ? xt : xmid - copysign(r, σ)
+
+        ynew = f(xnew)
+        if sign(y₂) == sign(ynew)
+            x₂, y₂ = xnew, ynew
+        elseif sign(y₁) == sign(ynew)
+            x₁, y₁ = xnew, ynew
+        else  # ynew == 0
+            return xnew
+        end
+        if abs(ynew) < ftol || abs(x₂ - x₁) < xtol
+            return (x₁ + x₂)/2
+        end
+    end
+    return (x₁ + x₂)/2
+end
